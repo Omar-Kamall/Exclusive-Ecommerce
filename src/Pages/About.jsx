d53056b56f2e5e1a9e2790784d464e0247f692a5
Omar-Kamall <ok9192750000000@gmail.com>
2025-04-25 17:42:06 +0300

    Frist Commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -18,25 +18,25 @@ const About = () => {
                 </div>
             </div>
             <section className="container mx-auto! px-[5%]! mt-25! grid grid-cols-2 lg:grid-cols-4 gap-5">
-                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl py-5!">
+                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl p-5!">
                     <img src={Img12} alt="Image-Error" loading='lazy'/>
                     <span className="font-extrabold">10.5k</span>
-                    <span className="">Sallers active our site</span>
+                    <span className="flex justify-center">Sallers active our site</span>
                 </div>
-                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl py-5!">
+                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl p-5!">
                     <img src={Img13} alt="Image-Error" loading='lazy'/>
                     <span className="font-extrabold">33k</span>
-                    <span className="">Mopnthly Produduct Sale</span>
+                    <span className="flex justify-center">Mopnthly Produduct Sale</span>
                 </div>
-                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl py-5!">
+                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl p-5!">
                     <img src={Img14} alt="Image-Error" loading='lazy'/>
                     <span className="font-extrabold">45.5k</span>
-                    <span className="">Customer active in our site</span>
+                    <span className="flex justify-center">Customer active in our site</span>
                 </div>
-                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl py-5!">
+                <div className="border-1 rounded flex flex-col justify-center items-center gap-3 shadow-2xl p-5!">
                     <img src={Img15} alt="Image-Error" loading='lazy'/>
                     <span className="font-extrabold">25k</span>
-                    <span className="">Anual gross sale in our site</span>
+                    <span className="flex justify-center">Anual gross sale in our site</span>
                 </div>
             </section>
             <section className="container mx-auto! px-[5%]! my-25! grid grid-cols-2 lg:grid-cols-4 gap-5">
